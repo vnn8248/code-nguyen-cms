32f9b07dcebc9674e81f0a18e1b7c0e4d5fc03a1
Bibi Westside <vnn8248@gmail.com>
2020-10-21 16:58:45 -0400

    Update server.js config

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env('PORT', 1337),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '12bc44a6fa7782867cedc55422f0a1e2'),
