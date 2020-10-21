803e88ba32b76ee3b52f6285127d8a2cda69c9df
Bibi Westside <vnn8248@gmail.com>
2020-10-20 22:02:36 -0400

    New database configs

---
+++ config/database.js
@@ -0,0 +1,40 @@
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "development") {
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'mongoose',
+          settings: {
+            host: env('DATABASE_HOST', '127.0.0.1'),
+            srv: env.bool('DATABASE_SRV', false),
+            port: env.int('DATABASE_PORT', 27017),
+            database: env('DATABASE_DEV', 'cms'),
+            username: env('DATABASE_USERNAME', null),
+            password: env('DATABASE_PASSWORD', null)
+          },
+          options: {
+            authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+            ssl: env.bool('DATABASE_SSL', false),
+          },
+        },
+      },
+    }
+} else {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "mongoose",
+          settings: {
+            uri: env("DATABASE_URI"),
+            database: env("DATABASE_NAME")
+          },
+          options: {
+            ssl: true
+          }
+        }
+      }
+    }
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '63a48804-d31b-4a99-b810-cdb6960d7209'
+};
