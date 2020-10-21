9eeb091397ea692ec27386a352105a4abd11607d
Bibi Westside <vnn8248@gmail.com>
2020-10-21 17:24:37 -0400

    Remove procfile and modify server.js

--- config/server.js
+++ config/server.js
@@ -1,9 +1,4 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '12bc44a6fa7782867cedc55422f0a1e2'),
-    },
-  },
+  port: env.int('PORT', 1337)
 });
