d4cc837f5a9cfde6fac159026d74b62a8c13c16f
Bibi Westside <vnn8248@gmail.com>
2020-10-26 11:53:30 -0400

    Update cloudinary config

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,13 @@
-module.exports = ({ env }) => ({
-  // ...
-  upload: {
-    provider: 'cloudinary',
-    providerOptions: {
-      cloud_name: env('CLOUDINARY_NAME'),
-      api_key: env('CLOUDINARY_KEY'),
-      api_secret: env('CLOUDINARY_SECRET'),
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    upload: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
     },
-  },
+  }
   // ...
-});
+};
