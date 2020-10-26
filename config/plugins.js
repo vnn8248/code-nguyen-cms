b340f12232c9cab8b1c1d6951054bdc2ae47830f
Bibi Westside <vnn8248@gmail.com>
2020-10-26 12:21:17 -0400

    Update environment config in plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,15 @@
 module.exports = ({ env }) => {
   if (env("NODE_ENV") === "production") {
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
-    },
+    return {
+      upload: {
+        provider: 'cloudinary',
+        providerOptions: {
+          cloud_name: env('CLOUDINARY_NAME'),
+          api_key: env('CLOUDINARY_KEY'),
+          api_secret: env('CLOUDINARY_SECRET'),
+        }
+      }
+    }
   }
   // ...
 };
