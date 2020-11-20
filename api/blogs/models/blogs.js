8faa66ecc68f3ac8f9c02f5c3169efa73fad5994
Bibi Westside <vnn8248@gmail.com>
2020-11-20 17:56:32 -0500

    Update slug logic for portfolio pieces

--- api/blogs/models/blogs.js
+++ api/blogs/models/blogs.js
@@ -2,14 +2,23 @@ const _ = require("lodash");
 
 module.exports = {
   lifecycles: {
+    // beforeSave: async (model, attrs, options) => {
+    //   if (options.method === "insert" && attrs.title) {
+    //     model.set("slug", _.kababCase(attrs.title));
+    //   } else if (options.method === "update" && attrs.title) {
+    //     attrs.slug = _.kebabCase(attrs.title);
+    //   }
+    // }
     beforeCreate: async (data) => {
-      if (data.title) {
+      if (!data.slug) {
         data.slug = _.kebabCase(data.title);
-        data.title = _.startCase(data.title);
       }
+      data.title = _.startCase(data.title);
     },
     beforeUpdate: async (params, data) => {
-      data.slug = _.kebabCase(data.title);
+      if (data.slug) {
+        data.slug = _.kebabCase(data.title);
+      }
       data.title = _.startCase(data.title);
     },
   },
--- api/portfolios/models/portfolios.js
+++ api/portfolios/models/portfolios.js
@@ -3,12 +3,14 @@ const _ = require("lodash");
 module.exports = {
   lifecycles: {
     beforeCreate: async (data) => {
-      if (data.name) {
+      if (!data.slug) {
         data.slug = _.kebabCase(data.name);
       }
     },
     beforeUpdate: async (params, data) => {
-      data.slug = _.kebabCase(data.name);
+      if (data.slug) {
+        data.slug = _.kebabCase(data.name);
+      }
     },
   },
 };
